dbc855729b6eb7ed282f643842e1ab5b890fae7d
Lachlan S <56066056+mmmmmmmmmmnm@users.noreply.github.com>
2020-05-29 16:55:02 +1000

    Add files via upload

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -9,16 +9,22 @@
 		<div class="form-group">
 			<input type="password" name="password" class="form-control" placeholder="Enter password" value="<?php if (get_cookie('upassword')) { echo get_cookie('upassword'); } ?>" required autofocus>
 		</div>
+		<div class="form-group">
+			<button type="submit" class="btn btn-primary">Login</button>
+		</div>
+		<div class="form-group">
+			<input type="checkbox" name="chkremember" value="Remember me" <?php if (get_cookie('uusername')) { ?> checked="checked" <?php } ?>> Remember Me
+		</div>
+		<div class="form-group">
+			<button type="button" class="btn btn-default">
+				<a href="<?php echo base_url()."users/forgot" ?>">Forgot password?</a>
+			</button>
+			<button type="button" class="btn btn-default">
+				<a href="<?php echo base_url()."users/register" ?>">Create account</a>
+			</button>
+		</div>
 	</div>
-	<button type="submit" class="btn btn-primary">Login</button>
 
-	<input type="checkbox" name="chkremember" value="Remember me" <?php if (get_cookie('uusername')) { ?> checked="checked" <?php } ?>> Remember Me
 
-	<button type="button" class="btn btn-default">
-		<a href="<?php echo base_url()."users/forgot" ?>">Forgot password?</a>
-	</button>
-	<button type="button" class="btn btn-default">
-		<a href="<?php echo base_url()."users/register" ?>">Create account</a>
-	</button>
 </div>
 <?php echo form_close(); ?>
