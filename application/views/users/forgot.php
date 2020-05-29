6824a028d82f84ad8efb011db48aefca13d82f80
Lachlan S <56066056+mmmmmmmmmmnm@users.noreply.github.com>
2020-05-29 16:49:00 +1000

    Add files via upload

--- application/views/users/forgot.php
+++ application/views/users/forgot.php
@@ -17,6 +17,9 @@
 		<h1 id="security_q" class="text-centre"></h1>
 		<hr>
 		<div class="form-group">
+
+
+			<input type="hidden" name="email" id="security_eid" />
 			<input type="text" name="security_a" id="security_a" class="form-control" placeholder="Bloggs" required autofocus>
 		</div>
 	</div>
@@ -27,13 +30,14 @@
 <?php echo form_open('', "id='password_reset' class='d-none'"); ?>
 <div class="row">
 	<div class="coll-md-4 col-md-offset-4">
-		<h1 id="password_reset" class="text-centre"></h1>
+		<h1 id="password_reset" class="text-centre">Enter new password: </h1>
 		<hr>
 		<div class="form-group">
-			<input type="text" name="new_pass" id="new_pass" class="form-control" placeholder="password" required autofocus>
+			<input type="hidden" name="email" id="resetpass_eid" />
+			<input type="password" name="new_pass" id="new_pass" class="form-control" placeholder="password" required autofocus>
 		</div>
 		<div class="form-group">
-			<input type="text" name="confirm_pass" id="confirm_pass" class="form-control" placeholder="password" required autofocus>
+			<input type="password" name="confirm_pass" id="confirm_pass" class="form-control" placeholder="password" required autofocus>
 		</div>
 	</div>
 	<button type="submit" class="btn btn-primary">Submit</button>
@@ -46,7 +50,7 @@
 	$(document).ready(function() {
 		$('#fpassw').submit(function(e){
 			e.preventDefault();
-			// $email = $('#email').val();
+			$email = $('#email').val();
 
 			$fdata = $(this).serialize();
 			// console.log($fdata);return;
@@ -55,6 +59,7 @@
 				success: function(response) {
 					console.log(response);
 					if(response.status) {
+						$('#security_eid').val( $email );
 						$('#security_q').text(response.security_q);
 						$('#fpassw').addClass('d-none');
 						$('#security_question').removeClass('d-none');
@@ -67,20 +72,18 @@
 
 
 
-		$('#fpassw').submit(function(e){
+		$('#security_question').submit(function(e){
 			e.preventDefault();
-			// $email = $('#email').val();
-
 			$fdata = $(this).serialize();
 			// console.log($fdata);return;
 			$.ajax({
-				type: "POST", url: base_url+'users/process_fpassw', data: $fdata, dataType: 'json',
+				type: "POST", url: base_url+'users/process_securityq', data: $fdata, dataType: 'json',
 				success: function(response) {
 					console.log(response);
 					if(response.status) {
-						$('#security_q').text(response.security_q);
-						$('#fpassw').addClass('d-none');
-						$('#security_question').removeClass('d-none');
+						$('#resetpass_eid').val( $('#security_eid').val() );
+						$('#security_question').addClass('d-none');
+						$('#password_reset').removeClass('d-none');
 					} else {
 						alert(response.error);
 					}
@@ -88,7 +91,22 @@
 			});
 		});
 
-
+		$('#password_reset').submit(function(e){
+			e.preventDefault();
+			$fdata = $(this).serialize();
+			//console.log($fdata);return;
+			$.ajax({
+				type: "POST", url: base_url+'users/reset_pass', data: $fdata, dataType: 'json',
+				success: function(response) {
+					console.log(response);
+					if(response.status) {
+						window.location.href = base_url+'users/login';
+					} else {
+						alert(response.error);
+					}
+				}
+			});
+		});
 
 	});
 
