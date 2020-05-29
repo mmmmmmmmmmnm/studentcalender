9e86ea24446aa0d29aabdefe8846118a3a9947e3
Lachlan S <56066056+mmmmmmmmmmnm@users.noreply.github.com>
2020-05-29 14:55:03 +1000

    Updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/studentCalender';
+$config['base_url'] = 'https://studentcalender.herokuapp.com/';
 //'http://localhost/studentCalender'
 //https://infs3202-3ca250d7.uqcloud.net/studentCalender
 /*
