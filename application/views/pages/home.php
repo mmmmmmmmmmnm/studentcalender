a8e9a5aaf9830c3087a648852a6bbe0b084ba2fd
Lachlan S <56066056+mmmmmmmmmmnm@users.noreply.github.com>
2020-06-03 08:53:52 +1000

    Update home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,13 +1,12 @@
 <h2><?= $title ?></h2>
 <p>
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros massa, hendrerit quis maximus ac, auctor quis ligula. In vestibulum ligula nec pulvinar vulputate. Pellentesque euismod, leo vitae dignissim fermentum, metus erat egestas libero, vestibulum rhoncus tellus sem et lectus. Nunc dolor tortor, sagittis a malesuada non, scelerisque ac est. Suspendisse convallis, risus id imperdiet blandit, felis nunc semper metus, ut laoreet dolor massa eu nisl. Mauris auctor nibh at nisl tristique mollis. Nullam eget libero sed felis pulvinar tristique eget ut ante. Proin efficitur interdum vestibulum. Fusce vehicula pharetra euismod. Maecenas dignissim ullamcorper dui.
-
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros massa, hendrerit quis maximus ac, auctor quis ligula. In vestibulum ligula nec pulvinar vulputate. Pellentesque euismod, leo vitae dignissim fermentum, metus erat egestas libero, vestibulum rhoncus tellus sem et lectus. Nunc dolor tortor, sagittis a malesuada non, scelerisque ac est. Suspendisse convallis, risus id imperdiet blandit, felis nunc semper metus, ut laoreet dolor massa eu nisl. Mauris auctor nibh at nisl tristique mollis. Nullam eget libero sed felis pulvinar tristique eget ut ante. Proin efficitur interdum vestibulum. Fusce vehicula pharetra euismod. Maecenas dignissim ullamcorper dui. 
+<br>
 Cras faucibus eget nunc a hendrerit. Vivamus egestas volutpat dolor vel dignissim. Maecenas rutrum varius lacus, quis varius mi viverra in. In hac habitasse platea dictumst. Etiam dapibus urna ante. Sed lectus magna, pharetra sollicitudin quam eget, euismod molestie neque. Proin non vehicula justo, dignissim commodo est. Quisque lectus purus, pellentesque id suscipit at, dapibus eget eros. Maecenas ultricies, nisi eu venenatis luctus, elit diam condimentum nisi, nec condimentum sapien ante in lectus. Pellentesque a ornare magna.
-
-Sed sed ipsum lorem. Morbi luctus velit est, sit amet tempor lorem hendrerit et. Pellentesque sit amet orci nulla. Donec accumsan mi non urna commodo, a auctor justo congue. Etiam accumsan aliquam lorem, vel gravida enim aliquet vel. Sed lacinia a orci id finibus. Nullam consequat dignissim elit ac blandit. Fusce ullamcorper rutrum rutrum.
-
-Aliquam efficitur consectetur est vitae rutrum. Curabitur et purus egestas, luctus ligula ut, placerat quam. Maecenas sit amet condimentum dolor. Cras quis dignissim urna. Ut et arcu metus. Aliquam ultricies lectus ut malesuada finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris et dolor augue. Nam id suscipit libero. Integer elit est, suscipit a hendrerit a, fermentum ac dui. Morbi pulvinar lacinia erat ut consectetur. Proin egestas risus dui, sit amet ornare nunc volutpat eget. Suspendisse potenti.
-
-Aliquam erat volutpat. Donec bibendum odio nibh. Nullam magna dui, aliquam nec suscipit et, tincidunt at urna. Proin ex augue, consectetur eu sem non, fermentum cursus ante. Nunc pulvinar nisl augue, sit amet vulputate lacus laoreet nec. Suspendisse potenti. Curabitur a purus luctus, laoreet nulla ut, euismod purus. </p>
-
+<br>
+Sed sed ipsum lorem. Morbi luctus velit est, sit amet tempor lorem hendrerit et. Pellentesque sit amet orci nulla. Donec accumsan mi non urna commodo, a auctor justo congue. Etiam accumsan aliquam lorem, vel gravida enim aliquet vel. Sed lacinia a orci id finibus. Nullam consequat dignissim elit ac blandit. Fusce ullamcorper rutrum rutrum. 
+<br>
+Aliquam efficitur consectetur est vitae rutrum. Curabitur et purus egestas, luctus ligula ut, placerat quam. Maecenas sit amet condimentum dolor. Cras quis dignissim urna. Ut et arcu metus. Aliquam ultricies lectus ut malesuada finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris et dolor augue. Nam id suscipit libero. Integer elit est, suscipit a hendrerit a, fermentum ac dui. Morbi pulvinar lacinia erat ut consectetur. Proin egestas risus dui, sit amet ornare nunc volutpat eget. Suspendisse potenti. 
+<br>
+Aliquam erat volutpat. Donec bibendum odio nibh. Nullam magna dui, aliquam nec suscipit et, tincidunt at urna. Proin ex augue, consectetur eu sem non, fermentum cursus ante. Nunc pulvinar nisl augue, sit amet vulputate lacus laoreet nec. Suspendisse potenti. Curabitur a purus luctus, laoreet nulla ut, euismod purus. 
+</p>
